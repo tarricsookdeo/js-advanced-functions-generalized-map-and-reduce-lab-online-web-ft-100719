dd16ae5e9b33b8bc3b1e022df244f9370c19a98c
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-03 19:05:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+const map = (sourceArray, func) => {
+  const newArray = [];
+  for (const n of sourceArray) {
+    newArray.push(func(n));
+  }
+  return newArray;
+};
+
+const reduce = (sourceArray, func, startingPoint = 0) => {
+  let memo, s;
+  if (startingPoint) {
+    memo = startingPoint;
+    s = 0;
+  } else {
+    memo = sourceArray[0];
+    s = 1;
+  }
+  for (let i = s; i < sourceArray.length; i++) {
+    memo = func(memo, sourceArray[i]);
+  }
+  return memo;
+};
